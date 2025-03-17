3d56c104cf990d59221c7b637903eb9fadb996a5
Franz Naling <nalingfranchesca@gmail.com>
2025-03-18 07:18:24 +1300

    Added new URL patterns, specifically for Login, Contact, Order, and Donut Details, apart from Home and Donuts page.

--- donut_shop_website/donuts/urls.py
+++ donut_shop_website/donuts/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
+    path('login/',views.login,name='login'),
+    path('contact/',views.contact,name='contact'),
+    path('order/',views.order,name='order'),
     path('donuts/',views.donuts,name='donuts'),
+    path('donuts/details/<int:id>',views.details,name='details'),
     path('',views.index,name='index'),
 ]
