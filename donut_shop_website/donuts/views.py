e26a85a29277eee8ed77aa817d35c07d9bfad6f7
Franz Naling <126060431+lemonfrance@users.noreply.github.com>
2026-05-18 06:12:07 +1200

    Heavily modified the ORDER VIEW to validate user form input, fill in missing field data accordingly (for foreign keys), and save the fully-cleaned input into the database. Also added a view to indicate order success.

--- donut_shop_website/donuts/views.py
+++ donut_shop_website/donuts/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.template import loader
-from .models import Flavour, Type
+from .models import Flavour, Type, Order, Customer, Ordered_Donuts
+from .forms import CustomerForm, Ordered_DonutsFormSet, TYPES, FLAVOURS
 
 # Create your views here.
 
@@ -30,17 +32,42 @@ def details(request, id):
     return HttpResponse(template.render(context,request))
 
 def order(request):
-    allTypes = Type.objects.all().values()
-    allFlavours = Flavour.objects.all().values()
-    
-    template = loader.get_template('order.html')
+    if request.method=="POST":
+        customerForm = CustomerForm(request.POST)
+        order = Order()
+        odFormset = Ordered_DonutsFormSet(request.POST,prefix='orderedDonuts',queryset=Ordered_Donuts.objects.none())
 
-    context = {
-        'types':allTypes,
-        'flavours':allFlavours,
-    }
+        # VALID customer
+        if customerForm.is_valid() and odFormset.is_valid(): 
+            customer = customerForm.save()  # Save customer to SQLite database
+            customerID = get_object_or_404(Customer, id = customer.pk)
 
-    return HttpResponse(template.render(context,request))
+            # VALID order
+            order.custID = customerID
+            order.save()
+            orderID = get_object_or_404(Order, id = order.pk)
+
+            # VALID ordered donuts
+            odFormset_instances = odFormset.save(commit=False)
+            for od in odFormset_instances:
+                typeID = TYPES.filter(id = od.typeID.pk)[0]
+                flavorID = FLAVOURS.filter(id = od.flavorID.pk)[0]
+
+                od.orderID = orderID
+                od.typeID = typeID
+                od.flavorID = flavorID
+                
+                od.save()
+            return redirect('order_success')
+    else: #if method == GET
+        customerForm = CustomerForm()
+        odFormset = Ordered_DonutsFormSet(prefix='orderedDonuts',queryset=Ordered_Donuts.objects.none())
+
+    return render(request, 'order.html', {'customer_form': customerForm, 'orderedDonuts_formset': odFormset})
+
+def order_success(request):
+    template = loader.get_template('successfulOrder.html')
+    return HttpResponse(template.render())
 
 def contact(request):
     template = loader.get_template('contact.html')
